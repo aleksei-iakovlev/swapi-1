f78a34332f98ee3dacb8b2c7ad679d24899dfd32
Aleksei Iakovlev <saint.alesha@ya.ru>
2026-01-14 15:53:18 +0300

    all done! finally

--- swapi.py
+++ swapi.py
@@ -7,46 +7,50 @@ class APIRequester:
     def __init__(self, base_url):
         self.base_url = base_url
 
-    def get(self, base_url):
+    def get(self, path):
+        base = self.base_url.rstrip('/')
+        endpoint = path.lstrip('/')
+        url = base + '/' + endpoint
         try:
-            response = requests.get(base_url)
+            response = requests.get(url)
             response.raise_for_status()
-            data = response.json()
-            return data
-        except requests.exceptions.RequestException as e:
-            return f'Ошибка {e}'
+            return response
+        except requests.exceptions.RequestException:
+            print('Возникла ошибка при выполнении запроса')
 
 
 class SWRequester(APIRequester):
-
-    base_url = 'https://swapi.dev/api/'
 
     def __init__(self, base_url):
 
         super().__init__(base_url)
 
-    def get_sw_categories(self, base_url):
+    def get_sw_categories(self):
 
-        response = APIRequester(base_url)
-        data = response.get(base_url)
+        data = requests.get(f'{self.base_url}/')
+        data = data.json()
         return data.keys()
 
     def get_sw_info(self, category):
 
-        response = APIRequester(self.base_url)
-        data = response.get(self.base_url + category)
-        return str(data)
+        response = self.get(f'{category}/')
+        return response.text
 
-    def save_sw_data(self):
-        object_sw_requester = SWRequester(self.base_url)
-        Path('data').mkdir(parents=True, exist_ok=True)
-        list_of_categories = object_sw_requester.get_sw_categories()
+
+def save_sw_data():
+    try:
+        sw_requester = SWRequester('https://swapi.dev/api')
+
+        Path('data').mkdir(exist_ok=True)
+        list_of_categories = sw_requester.get_sw_categories()
         for category in list_of_categories:
             with open(f'data/{category}.txt', 'w', encoding='utf-8') as f:
-                file = self.get_sw_info(category)
+                file = sw_requester.get_sw_info(category)
                 f.write(file)
+    except requests.exceptions.RequestException:
+        print('Возникла ошибка при выполнении запроса')
+# url for access https://swapi.py4e.com/api
+# url for pytest https://swapi.dev/api
 
 
-# sw_check = SWRequester('https://swapi.py4e.com/api/')
-# sw_check.save_sw_data()
-# print(sw_check.get_sw_categories('https://swapi.dev/api/'))
+save_sw_data()
